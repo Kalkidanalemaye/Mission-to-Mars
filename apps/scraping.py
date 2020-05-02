49a49f7f8a9ac5462b6aa0479a67ae879d38df17
Kalkidanalemaye <59233048+Kalkidanalemaye@users.noreply.github.com>
2020-05-02 12:34:25 -0400

    catching errors

--- apps/scraping.py
+++ apps/scraping.py
@@ -8,60 +8,82 @@ import pandas as pd
 
 browser = Browser('chrome', executable_path = 'chromedriver', headless=False)
 
+def mars_news(browser):
+    
+    # Vist the mars nasa news site.
+    url = ('https://mars.nasa.gov/news/')
+    browser.visit(url)
+    # Optional delay for loading page.
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time = 1)
 
-# # Visit the NASA mars news site
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = bs(html, "html.parser")
 
-url = 'https://mars.nasa.gov/news'
-browser.visit(url)
-browser.is_element_present_by_css('ul.item_list li.slide')
+    # Add try/except for error handling.
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
 
+        # Use the parent element to find the first `a` tag and save it as `news_title`.
+        news_title = slide_elem.find("div", class_='content_title').get_text()
 
-html = browser.html
-news_soup = bs(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide' , wait_time = 1)
+        # Use the parent element to find the summary paragraph.
+        news_p = slide_elem.find('div', class_ = 'article_teaser_body').get_text()
 
-slide_elem.find('div', class_='content_title')
+    except AttributeError:
+        return (None, None)
 
-news_title = slide_elem.find('div',class_='content_title').get_text()
-news_title
+    return (news_title, news_p)
 
+def featured_image(browser):
+    
+    # JPL Space Images Featured Image
+    url = ('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+    browser.visit(url)
 
-news_p = slide_elem.find('div',class_='article_teaser_body').get_text()
-news_p
+    # Find and click the full imagine button.
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
 
+    # Find the more info button and click it.
+    browser.is_element_present_by_text('more info', wait_time = 1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
 
-# # JPL Space Images Featured Image
+    # Parse the resulting html with soup.
+    html = browser.html
+    img_soup = bs(html, 'html.parser')
 
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
+    # Add try/except to handel errors.
+    try:
+        # Find the relative image url.
+        img_url_rel = img_soup.select_one('figure.lede a img').get('src')
 
-# Find and click the full image button
-full_image_elem = browser.find_by_id('full_image')
-full_image_elem.click()
+        # Use the base url to create an absolute url.
+        img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
 
-browser.is_element_present_by_text('more info', wait_time=1)
-more_info_elem = browser.find_link_by_partial_text('more info')
-more_info_elem.click()
+    except AttributeError:
+        return(None)
 
-html = browser.html
-img_soup = bs(html,'html.parser')
-
-img_url_rel = img_soup.select_one('figure.lede a img').get('src')
-img_url_rel
-
-img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-img_url
-
+    return (img_url)
 
 # # Mars Facts
 
-df = pd.read_html('http://space-facts.com/mars/')[0]
-df.columns=['description', 'value']
-df.set_index('description', inplace=True)
-df
+def mars_facts():
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('http://space-facts.com/mars/')[0]
 
-
-df.to_html()
+    except BaseException:
+        return None
+    
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+    
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
 
 browser.quit()
 
